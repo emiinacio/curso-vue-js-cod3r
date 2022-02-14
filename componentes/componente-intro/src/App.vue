52705686da00b7e7b0b8427e998d9cfd55a322b7
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-14 11:59:25 +0000

    .

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{ contador }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        contador: 0
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>

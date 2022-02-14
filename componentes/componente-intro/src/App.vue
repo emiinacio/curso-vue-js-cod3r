0740c13a9f28c839cc6e24873a3f1aac57cd3ca6
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-14 18:41:18 +0000

    update new components

--- componentes/componente-intro/src/App.vue
+++ componentes/componente-intro/src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div id="app">
-    <h1>{{ contador }}</h1>
+    <app-contadores />
+    <button>?</button>
   </div>
 </template>
 
 <script>
-export default {
-    data() {
-      return {
-        contador: 0
-      }
-    }
-}
+
 </script>
 
 <style>

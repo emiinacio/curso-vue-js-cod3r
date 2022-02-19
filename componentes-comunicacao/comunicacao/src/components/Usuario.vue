56a9debadf30f1f8a06a25dd287271814d3c74a8
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-19 20:12:51 +0000

    .

--- componentes-comunicacao/comunicacao/src/components/Usuario.vue
+++ componentes-comunicacao/comunicacao/src/components/Usuario.vue
@@ -2,10 +2,13 @@
     <div class="container">
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
-        
+        <p>Nome é <strong>{{nome}}</strong></p>
+        <button @click="alterarNome">Alterar Nome</button>
         <hr>
         <div class="componentes">
-            <app-usuario-info />
+            <app-usuario-info 
+            :nome="nome"
+            @nomeMudou="nome = $event"/>
             <app-usuario-editar />
         </div>
     </div>
@@ -16,7 +19,17 @@ import AppUsuarioInfo from './UsuarioInfo'
 import AppUsuarioEditar from './UsuarioEditar'
 
 export default {
-    components: { AppUsuarioInfo, AppUsuarioEditar }
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    data() {
+        return {
+            nome: 'Pedro'
+        }
+    },
+    methods: {
+        alterarNome() {
+            this.nome = 'Ana'
+        }
+    }
 }
 </script>
 
--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -2,12 +2,32 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong> {{ inverterNome() }} </strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        nome: {
+            type: String,
+            default: 'Anônimo',
+            //required: true
+            //default: function() {
+            //    return Array(10).fill(0).join(',')
+            //}
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
 }
 </script>
 
